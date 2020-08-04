c8c698d25a1bf3063bd99899fbdb914f84072b00
Matthew Fuqua <fuqua.matt@gmail.com>
2020-08-03 21:10:50 -0500

    Entity definitions

---
+++ BuzzBot/BuzzBotData/Data/LiveItemData.cs
@@ -0,0 +1,15 @@
+﻿namespace BuzzBotData.Data
+{
+    public class LiveItemData
+    {
+        public string FactionId { get; set; }
+        public Faction Faction { get; set; }
+        public string ItemId { get; set; }
+        public Item Item { get; set; }
+        public int MarketValue { get; set; }
+        public int HistoricalValue { get; set; }
+        public int MinimumBuyout { get; set; }
+        public int NumberOfAuctions { get; set; }
+        public int Quantity { get; set; }
+    }
+}
