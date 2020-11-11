1c8a3653bd6fce6401d77e31fb523f0dd0d7a72e
Matthew Fuqua <fuqua.matt@gmail.com>
2020-11-11 10:54:10 -0600

    Update on cost for Naxx Tier tokens

--- BuzzBot/BuzzBot/Epgp/EpgpCalculator.cs
+++ BuzzBot/BuzzBot/Epgp/EpgpCalculator.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using BuzzBot.Wowhead;
 using BuzzBotData.Data;
 
@@ -15,15 +16,78 @@ namespace BuzzBot.Epgp
         private const int CopperPerGp = 100000;
         public int ConvertGpFromGold(int totalCopper)
         {
-            return (int) Math.Ceiling((double) totalCopper / CopperPerGp);
+            return (int)Math.Ceiling((double)totalCopper / CopperPerGp);
         }
 
         public double CalculateItem(Item item, bool isHunter, bool isOffhand)
         {
+            if (TryParseTierToken(item, out var gpValue))
+            {
+                return gpValue;
+            }
             var iLevel = item.ItemLevel;
             var quality = item.QualityValue;
             var slot = item.InventorySlot;
             return Math.Pow(GetItemValue(iLevel, quality), 2) * 0.04 * GetSlotValue(slot, isHunter, isOffhand);
+        }
+
+        private class TokenData
+        {
+            public TokenData(int level, int slot, int quality)
+            {
+                ILevel = level;
+                Slot = slot;
+                Quality = quality;
+            }
+
+            public int ILevel { get; set; }
+            public int Slot { get; set; }
+            public int Quality { get; set; }
+        }
+
+        private Dictionary<int, TokenData> _naxxTokenDatas = new Dictionary<int, TokenData>
+        {
+            //Chest
+            {22351,new TokenData(88,5,4)},
+            {22350,new TokenData(88,5,4)},
+            {22349,new TokenData(88,5,4)},
+            //Legs
+            {22366,new TokenData(88,7,4)},
+            {22359,new TokenData(88,7,4)},
+            {22352,new TokenData(88,7,4)},
+            //Head
+            {22367,new TokenData(88,1,4)},
+            {22360,new TokenData(88,1,4)},
+            {22353,new TokenData(88,1,4)},
+            //Arms
+            {22369,new TokenData(88,9,4)},
+            {22362,new TokenData(88,9,4)},
+            {22355,new TokenData(88,9,4)},
+            //Hands
+            {22371,new TokenData(88,10,4)},
+            {22364,new TokenData(88,10,4)},
+            {22357,new TokenData(88,10,4)},
+            //Feet
+            {22372,new TokenData(88,8,4)},
+            {22365,new TokenData(88,8,4)},
+            {22358,new TokenData(88,8,4)},
+            //Belt
+            {22370,new TokenData(88,6,4)},
+            {22363,new TokenData(88,6,4)},
+            {22356,new TokenData(88,6,4)},
+            //Shoulders
+            {22368,new TokenData(88,3,4)},
+            {22361,new TokenData(88,3,4)},
+            {22354,new TokenData(88,3,4)},
+        };
+
+        private bool TryParseTierToken(Item item, out double gp)
+        {
+            gp = 0;
+            if (!_naxxTokenDatas.ContainsKey(item.Id)) return false;
+            var tokenData = _naxxTokenDatas[item.Id];
+            gp = Math.Pow(GetItemValue(tokenData.ILevel, tokenData.Quality), 2) * 0.04 * GetSlotValue(tokenData.Slot, false, false);
+            return true;
         }
 
         private double GetItemValue(int iLevel, int quality)
---
+++ BuzzBot/BuzzBot/Epgp/ItemResolver.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using BuzzBot.Discord.Services;
+using BuzzBotData.Data;
+using Discord.Commands;
+
+namespace BuzzBot.Epgp
+{
+    public class ItemResolver : IItemResolver
+    {
+        private readonly BuzzBotDbContext _dbContext;
+        private readonly IQueryService _queryService;
+        private readonly IAliasService _aliasService;
+        private readonly IItemMapper[] _itemMappers;
+
+        public ItemResolver(ItemMapperResolver mapperResolver, BuzzBotDbContext dbContext, IQueryService queryService, IAliasService aliasService)
+        {
+            _dbContext = dbContext;
+            _queryService = queryService;
+            _aliasService = aliasService;
+            _itemMappers = mapperResolver();
+        }
+        public async Task<Item> ResolveItem(Item toResolve, ICommandContext context, EpgpAlias targetAlias)
+        {
+            foreach (var mapper in _itemMappers)
+            {
+                if (!mapper.ContainsMap(toResolve)) continue;
+                var items = mapper.GetItems(toResolve, context, targetAlias).ToList();
+                if (items.Count <= 1)
+                {
+                    if (!items.Any())
+                    {
+                        await context.Channel.SendMessageAsync(
+                            "Unable to find any items that were eligible to be assigned to that user");
+                    }
+                    return items.FirstOrDefault();
+                }
+                return await QueryItem(items, context, targetAlias);
+            }
+
+            return toResolve;
+        }
+
+        private async Task<Item> QueryItem(List<Item> candidateItems, ICommandContext context, EpgpAlias targetAlias)
+        {
+            var index = await _queryService.SendOptionSelectQuery("Please select the item\n⚠️ - item has been claimed by this user previously\n", candidateItems,
+                item => GetQueryString(item, targetAlias), context.Channel, CancellationToken.None);
+            if (index == -1) return null;
+            return candidateItems[index];
+        }
+
+        private string GetQueryString(Item item, EpgpAlias alias)
+        {
+            var receivedPreviously = alias.AwardedItems.Any(itm => itm.ItemId == item.Id);
+            return $"{item.Name} {(receivedPreviously ? "⚠️" : string.Empty)}";
+        }
+    }
+}
